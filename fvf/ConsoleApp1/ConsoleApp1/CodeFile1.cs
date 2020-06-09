8bfd299ff3efcc34bb4460a5f8357675c458b706
dimaz0014 <58293091+dimaz0014@users.noreply.github.com>
2020-06-09 12:03:14 +0300

    Add files via upload

---
+++ fvf/ConsoleApp1/ConsoleApp1/CodeFile1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace struct_ceh
+{
+    struct Ceh
+    {
+        public string workerName;
+        public string cehName;
+        public int amountA;
+        public int amountB;
+        public int amountC;
+        public int priceA;
+        public int priceB;
+        public int priceC;
+
+        public Ceh(string allData)
+        {
+            var arr = allData.Split(' ');
+
+            workerName = arr[0] + " " + arr[1] + " " + arr[2];
+            cehName = arr[3];
+            amountA = Convert.ToInt32(arr[4]);
+            amountB = Convert.ToInt32(arr[5]);
+            amountC = Convert.ToInt32(arr[6]);
+            priceA = Convert.ToInt32(arr[7]);
+            priceB = Convert.ToInt32(arr[8]);
+            priceC = Convert.ToInt32(arr[9]);
+        }
+    }
+}
---
+++ fvf/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Http.Headers;
+using struct_ceh;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static Ceh[] ReadData(string fileName)
+        {
+            string path = "C:\\Users\\dimaz\\Desktop\\";
+            string newPath = path.Insert(path.Length, fileName);
+            int cnt = System.IO.File.ReadAllLines(newPath).Length;
+            Ceh[] works = new Ceh[cnt];
+            var stream = File.Open(newPath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(stream, System.Text.Encoding.Default);
+
+            for (int i = 0; i < cnt; i++)
+            {
+                var str = sr.ReadLine();
+                Ceh work = new Ceh(str);
+                works[i] = work;
+            }
+            return works;
+        }
+
+        static void kilkist(Ceh[] wrk, string worker1)
+        {
+            
+            for (int j = 0; j < wrk.Length; j++)
+            {
+                if (wrk[j].workerName == worker1)
+                {
+                    int aA = Convert.ToInt32(wrk[j].amountA);
+                    int aB = Convert.ToInt32(wrk[j].amountB);
+                    int aC = Convert.ToInt32(wrk[j].amountC);
+                    int total = aA + aB + aC;
+                    Console.WriteLine("Zahalna k-st vyrobiv = " + total);
+                }
+            }
+        }
+
+        static void second( Ceh[] wrk, string ch)
+        {
+            int sal = 0;
+            int wrkrsCount = 0;
+            for (int k = 0; k < wrk.Length; k++)
+            {
+                if(ch == wrk[k].cehName)
+                {
+                    int priceWA = Convert.ToInt32(wrk[k].amountA) * Convert.ToInt32(wrk[k].priceA);
+                    int priceWB = Convert.ToInt32(wrk[k].amountB) * Convert.ToInt32(wrk[k].priceB);
+                    int priceWC = Convert.ToInt32(wrk[k].amountC) * Convert.ToInt32(wrk[k].priceC);
+                    sal += priceWA + priceWB + priceWC;
+                    wrkrsCount++;
+                }
+            }
+            Console.WriteLine("Zarplata = " + sal);
+            Console.WriteLine("Serednya zarplata = " + sal/wrkrsCount );
+        }
+        static void Main(string[] args)
+        {
+            Ceh[] wrk = ReadData("data.txt");
+            Console.WriteLine("1 - Zahalna k-st vyrobiv");
+            Console.WriteLine("2 - Zarplata ta serednya");
+            int numb = Convert.ToInt32(Console.ReadLine());
+
+            switch(numb)
+            {
+                case 1:
+                    Console.WriteLine("Vvedit prizvysche ta inicialy vyrobnyka");
+                    string worker1 = Convert.ToString(Console.ReadLine());
+                    kilkist(wrk, worker1);
+                    break;
+                case 2:
+                    Console.WriteLine("vvedit Ceh");
+                    string ch = Convert.ToString(Console.ReadLine());
+                    second(wrk, ch);
+                    break;
+            }            
+            Console.ReadKey();
+        }
+    }
+}
